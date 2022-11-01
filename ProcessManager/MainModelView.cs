05db2a143dcbcc6f1c891957e9666d291dfdec8a
Jereml <Ledwojeremi@gmail.com>
2022-11-01 08:32:57 +0100

    Add automatic refresh, and selection of process

--- ProcessManager/MainModelView.cs
+++ ProcessManager/MainModelView.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace ProcessManager;
 
 public class MainModelView
 {
     private ObservableCollection<Process> _processes;
+    private DispatcherTimer _timer;
     public ObservableCollection<Process> processes
     {
         get { return _processes; }
@@ -16,12 +21,31 @@ public class MainModelView
             _processes = value;
         }
     }
+    
+    public Process? SelectedProcess { get; set; }
 
     public MainModelView()
     {
         processes = new ObservableCollection<Process>(Process.GetProcesses());
         RefreshCommand = new RelayCommand(Refresh);
+        StartAutomaticRefreshCommand = new RelayCommand(StartAutomaticRefresh);
+        PauseAutomaticRefreshCommand = new RelayCommand(PauseAutomaticRefresh, CanPauseAutomaticRefresh);
+        FilterCommand = new RelayCommand(Filter);
+        KillCommand = new RelayCommand(Kill, CanKill);
+        
+        _timer = new DispatcherTimer();
+        _timer.Tick += ((sender, args) => Refresh(null));
     }
+
+    private bool CanKill(object obj) => SelectedProcess != null;
+
+    private void Kill(object obj)
+    {
+        SelectedProcess.Kill();
+        SelectedProcess = null;
+        Refresh(null);
+    }
+
 
     private void Refresh(object obj)
     {
@@ -31,7 +55,38 @@ public class MainModelView
             processes.Add(process);
         }
     }
+    
+    private void PauseAutomaticRefresh(object obj)
+    {
+        _timer.Stop();
+    }
+    
+    private void StartAutomaticRefresh(object obj)
+    {
+        _timer.Stop();
+        _timer.Interval = TimeSpan.FromSeconds(Int32.Parse(obj.ToString()));
+        _timer.Start();
+    }
+    
+    private void Filter(object obj)
+    {
+        processes.Clear();
+        foreach (var process in Process.GetProcesses()
+                     .Where(p => p.ProcessName.IndexOf(obj.ToString(), StringComparison.OrdinalIgnoreCase) != -1))
+        {
+            processes.Add(process);
+        }
+    }
 
     public ICommand RefreshCommand { get; set; }
+    public ICommand StartAutomaticRefreshCommand { get; set; }
+    
+    public ICommand PauseAutomaticRefreshCommand { get; set; }
+    
+    public ICommand FilterCommand { get; set; }
+    public ICommand KillCommand { get; set; }
+    
+    private bool CanPauseAutomaticRefresh(object obj) => _timer.IsEnabled;
+
 
 }
--- ProcessManager/MainWindow.xaml.cs
+++ ProcessManager/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +26,12 @@ namespace ProcessManager
             InitializeComponent();
 
             DataContext = new MainModelView();
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
